adac3d872c1c678a22808eb7e4c4b37bbfbba2aa
Christiawan Eko S <christiawaneko@gmail.com>
2018-10-14 14:59:13 +0700

    Initial Commit

---
+++ Tab Bar View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tab Bar View
+//
+//  Created by Christiawan Eko Saputro on 14/10/18.
+//  Copyright © 2018 Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
