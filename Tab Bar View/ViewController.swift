2209cc800c7dc34abd839cd53c088555ad56ff6a
Christiawan Eko S <christiawaneko@gmail.com>
2018-10-26 11:42:00 +0700

    Simple Tab Bar

--- Tab Bar View/ViewController.swift
+++ Tab Bar View/ViewController.swift
@@ -13,8 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //        UITabBar.appearance().tintColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+        UITabBar.appearance().barTintColor = #colorLiteral(red: 0.1294117719, green: 0.2156862766, blue: 0.06666667014, alpha: 1)
+        
+        UITabBar.appearance().tintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        
     }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
 
